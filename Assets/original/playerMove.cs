66bfe8f0a984547a22b1e3d96e412204dc3f639f
rhenium75 <abc042615@gmail.com>
2018-07-11 11:50:30 +0900

    first commit
    first

---
+++ Assets/original/playerMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour {
+
+    private Rigidbody rig;
+
+    void Start () {
+        rig = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+
+    }
+}
